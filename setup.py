b659f5cdc6eb0f959cab2dce42ef9d278a3b1f4e
andtheysay <57023770+andtheysay@users.noreply.github.com>
2022-06-26 04:11:36 -0500

    fix typo

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        'torch,
+        'torch',
         'librosa',
         'museval',
         'h5py',
